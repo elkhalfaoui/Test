d01b2aeee214016f6381d04607711ecf26abfa18
= <elkhalfaouizakaria@gmail.com>
2025-02-28 20:08:33 +0000

    test

---
+++ Desktop/Tutorials/java/MyFirstClass.java
@@ -0,0 +1,7 @@
+public class MyFirstClass {
+    public static void main(String ...args) {
+        System.out.println("Hello, World!");
+        int a = 0;
+        System.out.println(a);
+    }
+}
---
+++ Desktop/Tutorials/java/Test.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+    int c;
+    printf("%d", c);
+    return 0;
+}

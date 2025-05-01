37470bdffed1195507f3b001d6cb5f826cde2cee
= <elkhalfaouizakaria@gmail.com>
2025-05-01 21:31:53 +0100

    commit

--- Desktop/Tutorials/java/MyFirstClass.java
+++ Desktop/Tutorials/java/MyFirstClass.java
@@ -1,7 +1,5 @@
 public class MyFirstClass {
-    public static void main(String ...args) {
-        System.out.println("Hello, World!");
-        int a = 0;
-        System.out.println(a);
+    public static void main(String []args) {
+        
     }
 }
--- Desktop/Tutorials/java/Test.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-int main() {
-    int c;
-    printf("%d", c);
-    return 0;
-}
